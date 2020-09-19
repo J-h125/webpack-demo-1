15a11016bd1b42e3fa8ffdfc15e374c9887e96f1
JH <473731589@qq.com>
2020-09-19 23:00:27 +0800

    准备上传

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
-var path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const base = require('./webpack.config.base.js')
 
 module.exports = {
-    mode: 'development',
-    entry:'./src/index.js',
-    output:{
-        filename:'index.js'
-    }
-
+    ...base,
+    
+    devtool:'inline-source-map',
+    devServer:{
+        contentBase:'./dist',
+    },
+    module: {
+        rules: [
+          ...base.module.rules,
+          {
+            test: /\.css$/i,
+            
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
 }
+
+
