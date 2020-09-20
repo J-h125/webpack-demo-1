29fea8ca1bae9395bd57e6dedf3e17d7d6f84c07
JH <473731589@qq.com>
2020-09-20 11:28:06 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import x from './x.js'
 import png from './assets/1.png'
 
-const div = document.getElementById('app')
-console.log(png)
-console.log('div')
-console.log(div)
-div.innerHTML = `
-    <img src="${png}">
+// const div = document.getElementById('app')
+// console.log(png)
+// console.log('div')
+// console.log(div)
+// div.innerHTML = `
+//     <img src="${png}">
 `
 const button = document.createElement('button')
 button.innerHTML = '懒加载'
